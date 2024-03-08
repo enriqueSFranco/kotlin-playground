8d2b252279462ba3bbb13383bff0557f9d57d3e8
enriqueSFranco <enrique.sfranco04@gmail.com>
2024-03-08 17:58:19 -0600

    algoritmos con kotlin

---
+++ src/main/kotlin/algorithms/sorting/quickSort.kt
@@ -0,0 +1,9 @@
+package org.tempest.algorithms.sorting
+
+/**
+ * Opciones para elegir el pivote
+ *
+ * Elegir siempre el primer elemento
+ * Elegir siempre el ultimo elemnto
+ * Elegir el medio como pivote
+ **/
