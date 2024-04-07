0892b1382330a91bc752e7e11be0391f96cb599c
enriqueSFranco <enrique.sfranco04@gmail.com>
2024-04-06 19:08:59 -0600

    ejercicio de leetcode

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,5 @@
 package org.tempest
 
-import org.tempest.algorithms.leetcode.BrowserHistory
-
 // TEST
 //println(simplifyPath("/a//b////c/d//././/.."))
 //println(simplifyPath("/a/./b/../../c/"))
@@ -10,13 +8,5 @@ import org.tempest.algorithms.leetcode.BrowserHistory
 //print(simplifyPath("/../"))
 
 fun main() {
-    val homepages = listOf<String>("google.com", "facebook.com", "youtube.com")
-    val browserHistory = BrowserHistory("leetcode.com")
 
-    for (homepage in homepages)
-        browserHistory.visit(homepage)
-
-
-    browserHistory.showBrowserHistory()
-    //println(browserHistory.back(1))
 }
