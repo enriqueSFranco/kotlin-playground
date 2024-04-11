9a9513fa68434d77c722f8a2d39cb1f4edd453b3
enriqueSFranco <enrique.sfranco04@gmail.com>
2024-04-10 20:01:23 -0600

    junit5

--- src/test/kotlin/algorithms/leetcode/SolutionTest.kt
+++ src/test/kotlin/algorithms/leetcode/SolutionTest.kt
@@ -1,12 +1,17 @@
 package algorithms.leetcode
 
-import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
+import org.tempest.algorithms.leetcode.Solution
+import kotlin.test.assertEquals
 
-@Tag("integrationTest")
 class SolutionTest {
+    private val solution = Solution()
 
     @Test
     fun test() {
+        assertEquals("/home/foo", solution.simplifyPath("/home//foo"))
+        assertEquals("/a/b/c", solution.simplifyPath("/a//b////c/d//././/.."))
+        assertEquals("/home", solution.simplifyPath("/home/"))
+        assertEquals("/", solution.simplifyPath("/../"))
     }
 }
