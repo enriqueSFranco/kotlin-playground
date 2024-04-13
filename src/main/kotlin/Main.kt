5e66c8750b69f10b033adff960c94ea56d8c7fe4
enriqueSFranco <enrique.sfranco04@gmail.com>
2024-04-12 19:39:19 -0600

    linked list

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,21 @@
 package org.tempest
 
-import org.tempest.dataStructure.stack.largestRectangleArea
-
+import org.tempest.dataStructure.LinkedList
 
 fun main() {
-    val heights = intArrayOf(2,1,5,6,2,3)
+    val myList = LinkedList<Int>()
 
-    print("is valid: ${largestRectangleArea(heights)}")
+    myList.addFirst(8)
+    myList.addFirst(9)
+    myList.add(10)
+    myList.add(11)
+    myList.add(12)
+    myList.add(100)
+    myList.add(2000)
+    myList.addLast(1000)
+
+    myList.showList()
+    println()
+    println("esta vacia? ${myList.isEmptyList()}")
+    println("size: ${myList.size()}")
 }
--- src/main/kotlin/dataStructure/linkedList/LinkedList.kt
+++ src/main/kotlin/dataStructure/linkedList/LinkedList.kt
@@ -3,8 +3,85 @@ package org.tempest.dataStructure
 data class ListNode<T>(val value: T, var next: ListNode<T>? = null)
 
 interface OutLinkedList<T> {
-
+    fun add(item: T)
+    fun addFirst(item: T)
+    fun addLast(item: T)
+    fun addAt(item: T, index: Int)
+    fun removeFirst(item: T)
+    fun removeLast(item: T)
+    fun removeAt(item: T)
+    fun removeAll(item: T)
+    fun size(): Int
+    fun isEmptyList(): Boolean
+    fun showList()
 }
 
-class LinkedList {
+class LinkedList<T>: OutLinkedList<T> {
+    private var head: ListNode<T>? = null
+    private var tail: ListNode<T>? = null
+    private var sizeList = 0
+
+    override fun isEmptyList(): Boolean = head == null
+
+    override fun add(item: T) {
+        var node = ListNode(item)
+        if (head == null) {
+            head = node
+            tail = node
+        }
+        tail?.next = node
+        tail = node
+        sizeList++
+    }
+
+    override fun addFirst(item: T) {
+        val node = ListNode(item)
+        if (head == null) {
+            head = node
+            tail = node
+        }
+        node.next = head
+        head = node
+    }
+
+    override fun addLast(item: T) {
+        val node = ListNode(item)
+        if (head == null) {
+            head = node
+            tail = node
+        }
+        tail?.next = node
+        tail = node
+    }
+
+    override fun addAt(item: T, index: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun removeAll(item: T) {
+        TODO("Not yet implemented")
+    }
+
+    override fun removeLast(item: T) {
+        TODO("Not yet implemented")
+    }
+
+    override fun removeFirst(item: T) {
+        TODO("Not yet implemented")
+    }
+
+    override fun removeAt(item: T) {
+        TODO("Not yet implemented")
+    }
+
+    override fun size(): Int = sizeList
+
+    override fun showList() {
+        var curr = head
+        while(curr != null) {
+            print(curr.value)
+            if (curr.next != null) print("->")
+            curr = curr.next
+        }
+    }
 }
