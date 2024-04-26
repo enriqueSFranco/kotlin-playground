d3974123c5bd88586550814ab911e8d2c8f85eba
enriqueSFranco <enrique.sfranco04@gmail.com>
2024-04-25 19:43:33 -0600

    feat: binary search tree

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,34 @@
 package org.tempest
 
-import org.tempest.algorithms.recursividad.factorial
+class Node(val data: Int, var left: Node? = null, var right: Node? = null) {}
 
+class BST {
+    var root: Node? = null
+
+    fun insert(data: Int) {
+        root = insertNode(root, data)
+    }
+
+    private fun insertNode(node: Node?, data: Int): Node {
+        if (node == null) {
+            return Node(data)
+        }
+        if (data < node.data) {
+            // insert to left
+            node.left = insertNode(node.left, data)
+        } else {
+            node.right = insertNode(node.right, data)
+        }
+        return node
+    }
+}
 
 fun main() {
-    val x = 5
-    print(factorial(x))
+    val values = listOf(5, 1, 3, 4, 2, 7)
+    val bst = BST()
+    var root: Node? = null
+
+    for (data in values) {
+        bst.insert(data)
+    }
 }
