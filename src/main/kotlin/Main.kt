fc80db2ac79adb93105a1f5517174c03bc5b74e9
enriqueSFranco <enrique.sfranco04@gmail.com>
2024-05-04 14:48:45 -0600

    feat: main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,34 +1,9 @@
 package org.tempest
 
-class Node(val data: Int, var left: Node? = null, var right: Node? = null) {}
-
-class BST {
-    var root: Node? = null
-
-    fun insert(data: Int) {
-        root = insertNode(root, data)
-    }
-
-    private fun insertNode(node: Node?, data: Int): Node {
-        if (node == null) {
-            return Node(data)
-        }
-        if (data < node.data) {
-            // insert to left
-            node.left = insertNode(node.left, data)
-        } else {
-            node.right = insertNode(node.right, data)
-        }
-        return node
-    }
-}
+import org.tempest.desingPatterns.EmployeeBuilder
 
 fun main() {
-    val values = listOf(5, 1, 3, 4, 2, 7)
-    val bst = BST()
-    var root: Node? = null
+   val employee = EmployeeBuilder.Builder("1", "", 28).build()
 
-    for (data in values) {
-        bst.insert(data)
-    }
+    print(employee)
 }
