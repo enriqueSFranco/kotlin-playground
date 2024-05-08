823aa1da33a0f5d5dd85010126feb2b81ebae2c9
enriqueSFranco <enrique.sfranco04@gmail.com>
2024-05-07 19:17:35 -0600

    feat: main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,16 @@
 package org.tempest
+import kotlin.time.measureTime
 
-import org.tempest.desingPatterns.EmployeeBuilder
+import org.tempest.algorithms.dynamicProgramming.coinChange
+
 
 fun main() {
-   val employee = EmployeeBuilder.Builder("1", "", 28).build()
-
-    print(employee)
+   //val employee = EmployeeBuilder.Builder("1", "", 28).build()
+    val coins = intArrayOf(186,419,83,408)
+    val amount = 6249
+    val time = measureTime {
+        val ans = coinChange(coins, amount)
+        println(ans)
+    }
+    println("El tiempo de ejecución fue de $time milisegundos.")
 }
