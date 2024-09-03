9e507819bb6140ec8f9992d6af6add7bb78836a2
enriqueSFranco <enrique.sfranco04@gmail.com>
2024-09-03 14:12:21 -0600

    feat(): add new topics

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,7 @@
 package org.tempest
 
-import org.tempest.roadmap.fileManagement.readFromFile
-import kotlin.time.measureTime
 
 fun main() {
     //val employee = EmployeeBuilder.Builder("1", "", 28).build()
     //val dateTimeString = "2024-04-30 02:25:00"
-
-    val time = measureTime {
-        readFromFile()
-    }
-    println("El tiempo de ejecución fue de $time milisegundos.")
 }
